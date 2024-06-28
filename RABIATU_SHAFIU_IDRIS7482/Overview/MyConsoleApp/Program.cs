42ece2e53c1dea9e47cabe8739a7660bb8a49648
rabia7482 <141380529+rabia7482@users.noreply.github.com>
2024-06-28 16:25:53 +0100

    COMMIT

---
+++ RABIATU_SHAFIU_IDRIS7482/Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 1;
+char c = 'A';
+float f = 123.45f;
+decimal d = 400.85m;
+int b = default;
+bool tf = default;
+
+Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
+
+string response;
+Console.WriteLine("What's your name?");
+response = Console.ReadLine();
+Console.WriteLine($"Enjoy the course,{response}");
+
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
---
+++ RABIATU_SHAFIU_IDRIS7482/Strings/Manipulation/Program.cs
@@ -0,0 +1,50 @@
+﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
+// Example file for manipulating string content
+
+string str1 = "The quick brown fox jumps over the lazy dog.";
+string str2 = "This is a string";
+string str3 = "THIS is a STRING";
+string[] strs = {"one", "two", "three", "four"};
+
+// TODO: Length of a string 
+Console.WriteLine(str1.Length);
+
+// TODO: Access individual characters
+Console.WriteLine(str1[14]);
+
+// TODO: Iterate over a string like any other sequence of values
+foreach (char ch in str1) {
+    Console.Write(ch);
+    if (ch=='b') {
+        Console.WriteLine();
+        break;
+    }
+}
+
+// TODO: String Concatenation         
+string outstr;
+outstr=string.Concat(strs);
+// TODO: Joining strings together with Join
+outstr=string.Join(',',strs);
+Console.WriteLine(outstr);
+outstr=string.Join("---", strs);
+Console.WriteLine(outstr);
+
+
+// TODO: String Comparison
+
+// Equals just returns a regular Boolean
+ bool isEqual = str2.Equals(str3);
+Console.WriteLine($"{isEqual}");
+
+// Compare will perform an ordinal comparison and return:
+// < 0 : first string comes before second in sort order
+// 0 : first and second strings are same position in sort order
+// > 0 : first string comes after the second in sort order
+
+int result=string.Compare(str2,"This is a string");
+Console.WriteLine($"{result}");
+
+// TODO: Replacing content
+string outstr1= str1.Replace("fox","cat");
+Console.WriteLine($"{outstr1}");
